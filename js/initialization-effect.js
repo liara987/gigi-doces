f30d75e14ca0b803bf55a67217afe3149621a8df
Liara <987.liara@gmail.com>
2020-05-29 16:44:52 -0300

    Init

---
+++ js/initialization-effect.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    // paralax effect
+    $('.parallax').parallax();
+
+    // materialboxed effect
+    $('.materialboxed').materialbox();
+
+    // sidenav effect
+    $('.sidenav').sidenav();
+
+    // slider effect
+    $('.slider').slider();
+
+    // modal effect    
+    $('.modal').modal();
+
+    // collapsible effect
+    $('.collapsible').collapsible();
+});
